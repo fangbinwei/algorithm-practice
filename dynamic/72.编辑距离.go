9711e4b218df8486123d80ae38a9d1a43d33020b
fangbinwei <fangbinwei@yeah.net>
2021-01-19 23:20:30 +0800

    docs(dynamic-programming): add 72

---
+++ dynamic/72.编辑距离.go
@@ -0,0 +1,110 @@
+package leetcode72
+
+/*
+ * @lc app=leetcode.cn id=72 lang=golang
+ *
+ * [72] 编辑距离
+ *
+ * https://leetcode-cn.com/problems/edit-distance/description/
+ *
+ * algorithms
+ * Hard (60.25%)
+ * Likes:    1357
+ * Dislikes: 0
+ * Total Accepted:    102.7K
+ * Total Submissions: 170.4K
+ * Testcase Example:  '"horse"\n"ros"'
+ *
+ * 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+ *
+ * 你可以对一个单词进行如下三种操作：
+ *
+ *
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：word1 = "horse", word2 = "ros"
+ * 输出：3
+ * 解释：
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：word1 = "intention", word2 = "execution"
+ * 输出：5
+ * 解释：
+ * intention -> inention (删除 't')
+ * inention -> enention (将 'i' 替换为 'e')
+ * enention -> exention (将 'n' 替换为 'x')
+ * exention -> exection (将 'n' 替换为 'c')
+ * exection -> execution (插入 'u')
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0
+ * word1 和 word2 由小写英文字母组成
+ *
+ *
+ */
+
+// @lc code=start
+// word1_i -> word2_j的编辑距离 dpij
+// if word1_i == word2_j, dp[i][j] = dp[i-1][j-1]
+// else
+// dp[i][j] = 1 + min{
+//	dp[i-1][j], // word1 删除i位置, 然后 i-1 -> j
+//	dp[i][j-1], // word1变换到j-1, 插入word2_j
+//	dp[i-1][j-1], // word1_i 替换成word2_j
+// }
+func minDistance(word1 string, word2 string) int {
+	dp := make([][]int, len(word1)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(word2)+1)
+	}
+
+	for i := 1; i < len(word2)+1; i++ {
+		dp[0][i] = i
+	}
+
+	for i := 1; i < len(word1)+1; i++ {
+		dp[i][0] = i
+	}
+	for i := 1; i < len(word1)+1; i++ {
+		for j := 1; j < len(word2)+1; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+			}
+		}
+	}
+	return dp[len(word1)][len(word2)]
+
+}
+
+func min(a, b, c int) int {
+	if a <= b && a <= c {
+		return a
+	}
+	if b <= a && b <= c {
+		return b
+	}
+	return c
+}
+
+// @lc code=end
